79d79eec4c0578a31e9969093f6f988657d5aeb8
Matheus Gurther <matheusgurther@outlook.com>
2025-09-10 17:24:58 -0300

    Utilizando props para listar tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 
 function App() {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: 'Estudar React',
+    description: 'Estudar os conceitos básicos de React',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: 'Estudar Node',
+    description: 'Estudar os conceitos básicos de Node',
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    title: 'Estudar JavaScript',
+    description: 'Estudar os conceitos básicos de JavaScript',
+    isCompleted: false,
+  }
+  ]);
+
   return (
-    <div>
-      <h1>Gerenciador de tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   )
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,9 @@
-function Tasks() {
+function Tasks(props) {
   return (
-    <div>
-        <h1>Tasks</h1>
-    </div> 
+        <ul className="space-y-4">
+            {props.tasks.map((task) => (
+            <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md ">{task.title}</li>))}
+        </ul>
   );
 }
 
